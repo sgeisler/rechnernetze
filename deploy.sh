0fb8d98287c76e75949a2bbdd3ecba19647d2c9d
Sebastian Geisler <sgeisler@agdsn.de>
2017-08-07 13:23:25 +0200

    fix travis (hopefully)

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 latexmk -pdf -interaction=nonstopmode main.tex
 git add -f main.pdf
 git commit -m "build pdf"
-git push -f https://sgeisler:$GITHUB_TOKEN@github.com/sgeisler/rechnernetze.git HEAD:gh-pages
+git push -f https://sgeisler:$1@github.com/sgeisler/rechnernetze.git HEAD:gh-pages
